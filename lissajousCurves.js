ee2e3b35419d74752e5d1fc95c60ad9db45151c9
F. I. Sohan <66239209+FISohan@users.noreply.github.com>
2021-12-17 01:16:14 +0600

    Lissajous Curves

--- lissajousCurves.js
+++ lissajousCurves.js
@@ -1,6 +1,3 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-
 var width = (canvas.width = window.innerWidth),
   height = (canvas.height = window.innerHeight);
 
